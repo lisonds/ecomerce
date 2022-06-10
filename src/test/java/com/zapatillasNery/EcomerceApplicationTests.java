8e452ae52b1142187b300abaad7c20bf4cec83c1
tulio <tulio.ore.27@unsch.edu.pe>
2022-06-09 22:40:37 -0500

    primercomit

---
+++ src/test/java/com/zapatillasNery/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapatillasNery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
